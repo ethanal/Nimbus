187d22db3f03434686b4a57c14fd50dfe6a45775
Ethan Lowman <ethan.l.527@gmail.com>
2014-07-26 01:50:06 -0400

    Fix problem with /media/show filtering

--- nimbus/apps/api/views.py
+++ nimbus/apps/api/views.py
@@ -18,9 +18,12 @@ class MultipleFieldLookupMixin(object):
     def get_object(self):
         queryset = self.get_queryset()             # Get the base queryset
         queryset = self.filter_queryset(queryset)  # Apply any filter backends
+       
         filter = {}
+        
         for field in self.lookup_fields:
-            filter[field] = self.kwargs[field]
+            if field in self.request.QUERY_PARAMS:
+                filter[field] = self.request.QUERY_PARAMS[field]
         return get_object_or_404(queryset, **filter)  # Lookup the object
 
 

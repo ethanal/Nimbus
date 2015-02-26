740d3a8cd082aca2d4b1865a42f4348e6ffb1ba7
Ethan Lowman <ethan.l.527@gmail.com>
2015-02-26 18:32:00 -0500

    Re-enable token auth

--- nimbus/settings/base.py
+++ nimbus/settings/base.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        #'rest_framework.authentication.TokenAuthentication',
+        "rest_framework.authentication.TokenAuthentication",
         #'rest_framework.authentication.SessionAuthentication',
         "nimbus.utils.SessionAuthentication",
     ),
